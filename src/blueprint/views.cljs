9edb31b6a003ba6da8cecfebd07942b96981857c
Brett Morgan <brett@brettmorgan.net>
2020-04-08 21:47:33 -0400

    Fixed view

--- src/blueprint/views.cljs
+++ src/blueprint/views.cljs
@@ -7,7 +7,7 @@
   [:div
    [:> Navbar
     [:> NavbarGroup {:align (. Alignment -LEFT)}
-     [:> NavbarHeading "Clojure Ledger"]
+     [:> NavbarHeading "Clojure Blueprint"]
      [:> NavbarDivider]
      [:> Button {:class-name "bp3-minimal" :icon "home" :text "Home"}]
      [:> Button {:class-name "bp3-minimal" :icon "document" :text "Files"}]]]
@@ -16,19 +16,4 @@
     [:> Card {:interactive true :elevation (. Elevation -THREE)}
      [:> H5 "Card Heading"]
      [:p  "This is where you put the content"]
-     [:> Button {:text "Bar" :intent "Success"}]]
-    [:> ControlGroup {:vertical false}
-     [:> InputGroup {:placeholder "Date"}]
-     [:> InputGroup {:placeholder "Code"}]
-     [:> InputGroup {:placeholder "Description"}]
-     [:> InputGroup {:placeholder "Comment"}]]
-    [:> ControlGroup {:vertical false}
-     [:> InputGroup {:placeholder "Status"}]
-     [:> InputGroup {:placeholder "Account"}]
-     [:> InputGroup {:placeholder "Amount"}]
-     [:> InputGroup {:placeholder "Comment"}]]
-    [:> ControlGroup {:vertical false}
-     [:> InputGroup {:placeholder "Status"}]
-     [:> InputGroup {:placeholder "Account"}]
-     [:> InputGroup {:placeholder "Amount"}]
-     [:> InputGroup {:placeholder "Comment"}]]]])
+     [:> Button {:text "Submit" :intent "Success"}]]]])
--- src/hello/cruel_world.cljs
+++
@@ -1,9 +0,0 @@
-(ns hello.cruel-world
-  (:require [bt]))
-
-(def app-db ())
-
-(defn what-kind? []
-  "Cruel")
-
-(js/console.log bt)
